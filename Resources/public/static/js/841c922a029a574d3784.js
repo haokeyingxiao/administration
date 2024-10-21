33db185dc97ea75cfdc226068229ff7a2fb263ef
gxiaosong <lyricsong47@gmail.com>
2024-10-21 17:36:44 +0000

    Merge branch refs/heads/shopware-trunk into trunk

--- Resources/public/static/js/841c922a029a574d3784.js
+++
@@ -1,1 +0,0 @@
-"use strict";(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[99921],{901818:function(n,o,t){t.r(o),t.d(o,{default:function(){return i}});var i={template:'\n{% block sw_cms_block_product_slider %}\n<div class="sw-cms-block-product-slider">\n    <slot name="productSlider"></slot>\n</div>\n{% endblock %}\n',compatConfig:Shopware.compatConfig}}}]);
